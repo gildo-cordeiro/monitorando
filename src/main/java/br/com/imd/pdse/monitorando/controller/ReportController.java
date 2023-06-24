6c7935c517e8cb4e5f39de462ab188f7e2df1399
Raryson Fred <raryson.fred2015@gmail.com>
2023-06-23 23:44:41 -0300

    folders, home, forum, framents/. corrections

--- src/main/java/br/com/imd/pdse/monitorando/controller/ReportController.java
+++ src/main/java/br/com/imd/pdse/monitorando/controller/ReportController.java
@@ -1,7 +1,6 @@
 package br.com.imd.pdse.monitorando.controller;
 
 import br.com.imd.pdse.monitorando.domain.User;
-import br.com.imd.pdse.monitorando.domain.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,24 +13,12 @@ public class ReportController {
 
     private static final String REPORT_PAGE = "report/report";
 
-    public String getGreeting() {
-        LocalTime currentTime = LocalTime.now();
-        String greeting;
-
-        if (currentTime.isAfter(LocalTime.of(0, 0)) && currentTime.isBefore(LocalTime.of(12, 0))) {
-            greeting = "Bom dia";
-        } else if (currentTime.isAfter(LocalTime.of(12, 0)) && currentTime.isBefore(LocalTime.of(18, 0))) {
-            greeting = "Boa tarde";
-        } else {
-            greeting = "Boa noite";
-        }
-        return greeting;
-    }
+    ClassroomController classroomController = new ClassroomController();
 
     @GetMapping("report/report")
     public String report(@ModelAttribute("user") User user, Model model) {
         model.addAttribute("user", user);
-        model.addAttribute("time", getGreeting());
+        model.addAttribute("time", classroomController.getGreeting());
         return REPORT_PAGE;
     }
 }
--- src/main/java/br/com/imd/pdse/monitorando/controller/TopicController.java
+++ src/main/java/br/com/imd/pdse/monitorando/controller/TopicController.java
@@ -1,17 +1,24 @@
 package br.com.imd.pdse.monitorando.controller;
 
+import br.com.imd.pdse.monitorando.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.time.LocalTime;
 
 @Controller
 public class TopicController {
 
-    private static final String TOPIC_PAGE = "forum/topic";
+    private static final String TOPIC_PAGE = "report/topic";
 
-    @GetMapping("/topic")
-    public String topic(Model model) {
-        model.addAttribute("titulo", "Topic");
+    ClassroomController classroomController = new ClassroomController();
+
+    @GetMapping("report/topic")
+    public String topic(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("time", classroomController.getGreeting());
         return TOPIC_PAGE;
     }
 }
