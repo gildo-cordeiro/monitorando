ef37fa3a0ba8e79ed5103d3bfde74a0712ee6a9d
Raryson Fred <raryson.fred2015@gmail.com>
2023-06-30 23:24:28 -0300

    updates

--- src/main/java/br/com/imd/pdse/monitorando/controller/LoginController.java
+++ src/main/java/br/com/imd/pdse/monitorando/controller/LoginController.java
@@ -3,12 +3,14 @@ package br.com.imd.pdse.monitorando.controller;
 import br.com.imd.pdse.monitorando.domain.User;
 import br.com.imd.pdse.monitorando.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController {
@@ -57,5 +59,18 @@ public class LoginController {
 
         model.addAttribute("user", new User());
         return REGISTER_PAGE;
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth != null) {
+                new SecurityContextLogoutHandler().logout(request, response, auth);
+            }
+            return "redirect:/login?logout";
+        } catch (Exception e) {
+            return "redirect:/login?error";
+        }
     }
 }
